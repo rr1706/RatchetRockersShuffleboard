db1b98c2d5b97964daeb7456b0577e0b4f60a858
William Behrens <35979547+WilliamBehrens@users.noreply.github.com>
2022-01-17 17:38:21 -0800

    Create boilerplate

---
+++ src/main/java/frc/rr1706/RatchetTheme.java
@@ -0,0 +1,7 @@
+import edu.wpi.first.shuffleboard.api.plugin.Description;
+import edu.wpi.first.shuffleboard.api.plugin.Plugin;
+
+@Description(group = "frc.rr1706", name = "RatchetTheme", version = "0.0.1", summary = "Ratchet Rockers Shuffleboard theme as a plugin")
+public class RatchetTheme extends Plugin {
+
+}
