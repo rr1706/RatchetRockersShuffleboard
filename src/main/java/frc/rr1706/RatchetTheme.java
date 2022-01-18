f9be006b175a3a67b9fad75d05d4d87027689ff9
driverStation1706 <emersonohara1706@gmail.com>
2022-01-18 17:05:30 -0600

    save

--- src/main/java/frc/rr1706/RatchetTheme.java
+++ src/main/java/frc/rr1706/RatchetTheme.java
@@ -1,7 +1,10 @@
+package frc.rr1706;
+
 import edu.wpi.first.shuffleboard.api.plugin.Description;
 import edu.wpi.first.shuffleboard.api.plugin.Plugin;
+import edu.wpi.first.shuffleboard.api.theme.Theme;
 
 @Description(group = "frc.rr1706", name = "RatchetTheme", version = "0.0.1", summary = "Ratchet Rockers Shuffleboard theme as a plugin")
 public class RatchetTheme extends Plugin {
-
+    private static final Theme myTheme = new Theme(RatchetTheme.class, "Ratchet", RatchetTheme.class.getResource("/Ratchet.css").toString());
 }
