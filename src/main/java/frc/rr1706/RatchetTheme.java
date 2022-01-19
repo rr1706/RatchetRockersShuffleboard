30fc1bfef4355793c6256a2a4cda1a288fec7b28
driverStation1706 <emersonohara1706@gmail.com>
2022-01-18 19:44:36 -0600

    fix boilerplate and theme loading

--- src/main/java/frc/rr1706/RatchetTheme.java
+++ src/main/java/frc/rr1706/RatchetTheme.java
@@ -1,10 +1,17 @@
 package frc.rr1706;
 
-import edu.wpi.first.shuffleboard.api.plugin.Description;
-import edu.wpi.first.shuffleboard.api.plugin.Plugin;
+import edu.wpi.first.shuffleboard.api.plugin.*;
 import edu.wpi.first.shuffleboard.api.theme.Theme;
 
+import java.util.*;
+
+//@Requires (group = "edu.wpi.first.shuffleboard", name = "Base", minVersion = "1.3.0")
 @Description(group = "frc.rr1706", name = "RatchetTheme", version = "0.0.1", summary = "Ratchet Rockers Shuffleboard theme as a plugin")
-public class RatchetTheme extends Plugin {
-    private static final Theme myTheme = new Theme(RatchetTheme.class, "Ratchet", RatchetTheme.class.getResource("/Ratchet.css").toString());
+public final class RatchetTheme extends Plugin {
+    private final Theme myTheme = new Theme(getClass(), "Ratchet", "Ratchet.css");
+
+    @Override 
+    public List<Theme> getThemes(){
+        return List.of(myTheme);
+    }
 }
