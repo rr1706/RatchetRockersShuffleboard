a9b853546cceae02151976178b8f5191996eea0c
William Behrens <35979547+WilliamBehrens@users.noreply.github.com>
2022-01-24 12:00:25 -0600

    Update RatchetTheme.kt

--- src/main/java/frc/rr1706/RatchetTheme.kt
+++ src/main/java/frc/rr1706/RatchetTheme.kt
@@ -1,17 +1,16 @@
-package frc.rr1706;
+package frc.rr1706
 
-import edu.wpi.first.shuffleboard.api.plugin.*;
-import edu.wpi.first.shuffleboard.api.theme.Theme;
+import edu.wpi.first.shuffleboard.api.plugin.*
+import edu.wpi.first.shuffleboard.api.theme.Theme
 
-import java.util.*;
+import java.util.*
 
 //@Requires (group = "edu.wpi.first.shuffleboard", name = "Base", minVersion = "1.3.0")
 @Description(group = "frc.rr1706", name = "RatchetTheme", version = "0.0.1", summary = "Ratchet Rockers Shuffleboard theme as a plugin")
-public final class RatchetTheme extends Plugin {
-    private final Theme myTheme = new Theme(getClass(), "Ratchet", "Ratchet.css");
+class RatchetTheme : Plugin {
+    private val m_customTheme: Theme = Theme(getClass(), "Ratchet", "Ratchet.css")
 
-    @Override 
-    public List<Theme> getThemes(){
-        return List.of(myTheme);
+    override fun getThemes(): List<Theme>{
+        return List.of(m_customTheme)
     }
 }
