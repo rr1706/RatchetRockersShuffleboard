ed1c08a3cb9ad5b60c566b5f1c3969f50495b005
William Behrens <35979547+WilliamBehrens@users.noreply.github.com>
2022-01-24 12:04:13 -0600

    Update UDPCameraViewer.kt

--- src/main/java/frc/rr1706/UDPCameraViewer.kt
+++ src/main/java/frc/rr1706/UDPCameraViewer.kt
@@ -1,16 +1,16 @@
-package frc.rr1706;
+package frc.rr1706
 
-import edu.wpi.first.shuffleboard.api.data.DataType;
-import edu.wpi.first.shuffleboard.api.plugin.Description;
-import edu.wpi.first.shuffleboard.api.plugin.Plugin;
-import edu.wpi.first.shuffleboard.api.widget.ComponentType;
-import edu.wpi.first.shuffleboard.api.widget.WidgetType;
+import edu.wpi.first.shuffleboard.api.data.DataType
+import edu.wpi.first.shuffleboard.api.plugin.Description
+import edu.wpi.first.shuffleboard.api.plugin.Plugin
+import edu.wpi.first.shuffleboard.api.widget.ComponentType
+import edu.wpi.first.shuffleboard.api.widget.WidgetType
 
-import frc.rr1706.widgets.UDPCameraViewerWidget;
-import frc.rr1706.data.UDPCameraFeedType;
+import frc.rr1706.widgets.UDPCameraViewerWidget
+import frc.rr1706.data.UDPCameraFeedType
 
-import java.util.List;
-import java.util.Map;
+import java.util.List
+import java.util.Map
 
 @Description(
     group = "frc.rr1706", 
@@ -18,25 +18,16 @@ import java.util.Map;
     version = "0.1.0", 
     summary = "A widget for viewing a camera/image feed from a udp connection"
 )
-public final class UDPCameraViewer extends Plugin {
-    @Override
-    public List<DataType> getDataTypes() {
-        return List.of(
-            UDPCameraFeedType.Instance
-        );
+class ImageViewer : Plugin {
+    override fun getDataTypes(): List<DataType> {
+        return List.of(ImageType.Instance)
     }
 
-    @Override
-    public List<ComponentType> getComponents() {
-        return List.of(
-            WidgetType.forAnnotatedWidget(UDPCameraViewerWidget.class)
-        );
+    override fun getComponents(): List<ComponentType> {
+        return List.of(WidgetType.forAnnotatedWidget(ImageViewerWidget.class))
     }
 
-    @Override
-    public Map<DataType, ComponentType> getDefaultComponents() {
-        return Map.of(
-            UDPCameraFeedType.Instance, WidgetType.forAnnotatedWidget(UDPCameraViewerWidget.class)
-        );
+    override fun getDefaultComponents(): Map<DataType, ComponentType> {
+        return Map.of(ImageType.Instance, WidgetType.forAnnotatedWidget(ImageViewerWidget.class))
     }
 }
